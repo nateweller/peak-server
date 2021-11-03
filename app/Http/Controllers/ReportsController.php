63eceb15dd3e8d6caa46db1cde786b5a1204c24d
nateweller <hello@nateweller.com>
2021-11-02 18:50:24 -0600

    development check-in

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -9,10 +9,11 @@ class ReportsController extends Controller
 
     public function gradeSummary()
     {
-        return DB::table('climbs')
-                    ->leftJoin('grading_grades', 'grading_grades.id', '=', 'climbs.grade_id')
-                    ->select('climbs.grade_id', 'grading_grades.name', DB::raw('count(*) as count'))
-                    ->groupBy('climbs.grade_id')
+        return DB::table('grading_grades')
+                    ->leftJoin('climbs', 'climbs.grade_id', '=', 'grading_grades.id')
+                    ->leftJoin('grading_systems', 'grading_grades.grading_system_id', '=', 'grading_systems.id')
+                    ->select('grading_systems.discipline', 'grading_grades.id', 'grading_grades.name', DB::raw('count(climbs.grade_id) as count'))
+                    ->groupBy('grading_grades.id')
                     ->get();
     }
 
